42031cc2c09561e74ecb2a465532f61b58cb0203
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2020-09-04 21:23:01 +0530

    Comment Component and View created
    A component and a view for the comments page of the reddit post have
    beeb created

---
+++ src/components/RedditComments.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        yo
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class RedditComment extends Vue {
+    showSubmissionId(): void {
+        console.log(this.$route.params.id);
+    }
+
+    mounted() {
+        this.showSubmissionId();
+    }
+}
+</script>
---
+++ src/views/Comments.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    all comments components
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Comment extends Vue {
+  showSubmissionId(): void {
+    console.log(this.$route.params);
+  }
+
+  mounted() {
+    this.showSubmissionId();
+  }
+}
+</script>
